afcd57e8998c394a72debe65ef4307861c9b8089
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-03-14 13:20:12 -0700

    styled Output component - mobile

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Output.css'
 
 function Output() {
   return (
-    <div>Output</div>
+    <section className='output'>
+      <div className='tip-amount'>
+        <div className='text'>
+          <p>Tip Amount</p>
+          <p>/ person</p>
+        </div>
+        <p className='amount'>$5.45</p>
+      </div>
+      <div className='total'>
+      <div className='text'>
+          <p>Total</p>
+          <p>/ person</p>
+        </div>
+        <p className='amount'>$5.45</p>
+      </div>
+      <button>Reset</button>
+    </section>
   )
 }
 
